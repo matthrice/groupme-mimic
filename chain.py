de12f5f7208e7c83f3bd1381c59186b3f210584c
matthrice <mattrice@umich.edu>
2018-01-06 16:56:42 -0500

    chain and generate

---
+++ chain.py
@@ -0,0 +1,68 @@
+import pickle
+import sys
+import random
+
+filenames = []
+text_data = []
+markov_model = {}
+
+"""Get command line file arguments"""
+try:
+    filenames = sys.argv[1:]
+except:
+    print("There must be at least one text")
+    sys.exit(2)
+
+"""Open files"""
+try:
+    for filename in filenames:
+        temp_file = open("filename", "r")
+        text_data.append(temp_file)
+except:
+    print("Could not open file")
+    sys.exit(2)
+
+
+
+
+def build_markov(text_data, markov_model):
+    """Build model using markov chains"""
+
+    for line in text_data:
+    line = line.lower().split()
+    for i, word in enumerate(line):
+        if i == len(line) - 1:
+            markov_model['END'] = model.get('END', []).append(word)
+        else:
+            if i == 0:
+                markov_model['START'] = model.get('START', []).append(word)
+            model[word] = model.get(word, []).append(line[i + 1])
+
+def save_model(markov_model, filename):
+    """Save model as 'markov_model.pickle' """
+
+    pickle_out = open(filename, "wb")
+    pickle.dump(markov_model, pickle_out)
+    pickle_out.close()
+
+def load_model(markov_model, filename):
+    """Load model from specified filename"""
+
+    pickle_in = open(filename, "rb")
+    markov_model = pickle.load(pickle_in)
+
+
+def generate_sentence(markov_model):
+    """Use markov chain to generate sentences"""
+
+    generated = []
+    while True:
+        if not generated:
+            words = markov_model['START']
+        elif generated[-1] in markov_model['END']:
+            break
+        else:
+            words = model[generated[-1]]
+        generated.append(random.choice(words))
+    
+    return generated.join(" ")
