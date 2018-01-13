d0ff52544a15006b9fb9cac656389d2aedbe348a
matthrice <mattrice@umich.edu>
2018-01-13 14:57:54 -0500

    add create_history

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # remark-ov
 Markov chains to generate sentences by modeling the contents of two texts
+
+Complete with a groupme scraper to model a groupme user
--- lib/scrape/groupme.py
+++ lib/scrape/groupme.py
@@ -26,4 +26,64 @@ def get_url(token, chat_type, chat_ID):
 
     return url
 
+def get_json(url):
+    """Retrieve json from url"""
+    res = urllib2.urlopen(url)
+    json = load(res)
+
+    return json
+
+def get_self_id(token):
+    """Obtain a user's ID given token"""
+    url = "https://api.groupme.com/v3/users/me?token={}".format(token)
+    json = get_json(url)
+    user_id = json['response']['user_id']
+
+    return user_id
+
+def get_groups(token):
+    """Return a list of group chats' IDs and names."""
+    url = 'https://api.groupme.com/v3/groups?token={}'.format(token)
+    json = get_json(url)
+    response = json['response']
+    
+    groups = []
+    for i in response:
+        ID = i['id']
+        name = i['name']
+        groups.append([ID, name])
+
+    return groups
+
+def get_directs(token):
+    """Return a list of direct message chats' IDs and names."""
+    url = 'https://api.groupme.com/v3/chats?token={}'.format(token)
+    json = get_json(url)
+    response = json['response']
+    
+    directs = []
+    for i in response:
+        ID = i['other_user']['id']
+        name = i['other_user']['name']
+        directs.append([ID, name])
+
+    return directs
+
+def create_history(json, url, self_id, chat_type, chat_ID, msg_count, msg_limit):
+    """ Create a full chat history
+
+    @json - GroupMe API response in JSON format
+    @url - URL being worked with
+    @self_id - user's GroupMe ID
+    @chat_type - the type of chat ('group' or 'direct')
+    @chat_id - the chat's id
+    @msg_count - total number of messages in the chat
+    @msg_limit - number of messages received in a set
+    """
+
+    
+
+
+    
+
 
